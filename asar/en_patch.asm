34f6c2d503699c3a0d30d0e660344f593e69d3ad
unknown <cleartonic@gmail.com>
2021-04-21 23:57:19 -0500

    update

--- asar/en_patch.asm
+++
@@ -1,4 +0,0 @@
-hirom
-;force english to always skip translation splash screen
-org $C0ff9C
-jml $C004A4
--- asar/patch.sh
+++
@@ -1,2 +0,0 @@
-cp "E:\pmac_junk_rev_1\emulators\DQIII\ROM\dq3_en_patch.sfc" "E:\pmac_junk_rev_1\emulators\DQIII\ROM\dq3_en_patch_r.sfc"
-"E:\pmac_junk_rev_1\emulators\DQIII\asar.exe" --fix-checksum=off "patch.asm" "E:\pmac_junk_rev_1\emulators\DQIII\ROM\dq3_en_patch_r.sfc"
